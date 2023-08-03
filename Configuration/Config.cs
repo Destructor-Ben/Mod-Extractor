e3944ccef2045fe3070f47f7f12c1eac811c3c09
Destructor_Ben <ben.honeyfield@gmail.com>
2023-08-03 19:11:32 +1200

    changes

--- Configuration/Config.cs
+++ Configuration/Config.cs
@@ -1,7 +1,7 @@
+#pragma warning disable CS0649
 using System.ComponentModel;
 using Terraria.ModLoader.Config;
 
-#pragma warning disable CS0649
 namespace ExtractAnyMod.Configuration;
 internal class Config : ModConfig
 {
@@ -13,4 +13,3 @@ internal class Config : ModConfig
     [DefaultValue(true)]
     public bool ExtractResources;
 }
-#pragma warning restore CS0649
