5c079880f3b6ee1f082bd6d4c5e57b475ebe1072
Destructor_Ben <ben.honeyfield@gmail.com>
2023-08-22 13:56:42 +1200

    Updating

---
+++ Content/ExtractEdit.cs
@@ -0,0 +1,33 @@
+﻿using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using System.Reflection;
+using TerraUtil.Edits;
+
+namespace ExtractAnyMod.Content;
+internal class ExtractEdit : ILEditReflection
+{
+    private static Type BuildProperties => typeof(Main).Assembly.GetType("Terraria.ModLoader.Core.BuildProperties") ?? throw new Exception("'BuildProperties' is null");
+    private static MethodInfo ExtractMethod => (typeof(Main).Assembly.GetType("Terraria.ModLoader.UI.UIExtractMod")?.GetMethod("Extract", BindingFlags.NonPublic | BindingFlags.Instance)) ?? throw new Exception("'ExtractMethod' is null");
+    private static FieldInfo HideCode => BuildProperties.GetField("hideCode", BindingFlags.Instance | BindingFlags.NonPublic) ?? throw new Exception("'hideCode' is null");
+    private static FieldInfo HideResources => BuildProperties.GetField("hideResources", BindingFlags.Instance | BindingFlags.NonPublic) ?? throw new Exception("'hideResources' is null");
+
+    public override MethodInfo Method => ExtractMethod;
+
+    public override void Apply(ILCursor c)
+    {
+        while (c.TryGotoNext(i => i.MatchLdfld(HideCode)))
+        {
+            c.Index++;
+            c.Emit(OpCodes.Pop);
+            c.EmitDelegate(delegate () { return !Config.Instance.ExtractCode; });
+        }
+
+        c.Index = 0;
+        while (c.TryGotoNext(i => i.MatchLdfld(HideResources)))
+        {
+            c.Index++;
+            c.Emit(OpCodes.Pop);
+            c.EmitDelegate(delegate () { return !Config.Instance.ExtractResources; });
+        }
+    }
+}
